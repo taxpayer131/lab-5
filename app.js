bb9316267509f901b50c397d266dec28cddbd069
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-09 15:13:07 -0700

    prob5 solved

--- app.js
+++ app.js
@@ -89,10 +89,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var multDigits = multiply(testArray[0], testArray[1]);
+  var totalDigits = multiply(multDigits, testArray[2]);
+  var digitArray = 'The numbers 2,3,4 have a product of 24.';
+  return [totalDigits, digitArray];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+multiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
